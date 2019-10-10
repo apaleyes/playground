2066a5328d53a06d9e6772a80dcd3a99223a93c8
Andrei Paleyes <apalees@gmail.com>
2019-10-11 00:02:35 +0100

    Moved mcts methods to a class

--- AlphaToe/mcts.py
+++ AlphaToe/mcts.py
@@ -177,90 +177,108 @@ class Node:
         return v / n + UCB_C * math.sqrt(math.log(N) / n)
 
 
-def loop(game, n_iterations, debug=False):
-    root = Node(game.get_initial_position())
-
-    for i in range(n_iterations):
-        print("Starting iteration " + str(i))
-        
-        leaf_node = select(root, game)
-        if leaf_node is None:
-            print("All possible positions were evaluated")
-            break
-
-        new_child = expand(leaf_node, game)
-        outcome = simulate(new_child, game)
-        backpropagate(new_child, outcome)
-
-    if debug:
-        print_tree(root)
+class TreeSearch:
+    def __init__(self, game):
+        self.game = game
 
 
-def select(node, game):
-    if len(node.children) == 0:
-        # found a leaf, return it
-        return node
+    def loop(self, n_iterations, debug=False):
+        root = Node(self.game.get_initial_position())
 
-    non_terminal_children = [child for child in node.children if not game.is_terminal(child.position)]
-    # sort in decsending order
-    non_terminal_children.sort(key=lambda c: c.ucb(), reverse=True)
+        for i in range(n_iterations):
+            print("Starting iteration " + str(i))
 
-    # iterate over sorted children, finish when
-    # we found a non-terminal leaf
-    for child in non_terminal_children:
-        leaf = select(child, game)
-        if leaf is not None:
-            return leaf
+            leaf_node = self.select(root)
+            if leaf_node is None:
+                print("All possible positions were evaluated")
+                break
 
-    # we haven't found a non-terminal leaf in this subtree
-    return None
+            new_child = self.expand(leaf_node)
+            outcome = self.simulate(new_child)
+            self.backpropagate(new_child, outcome)
 
-
-def expand(leaf_node, game):
-    next_positions = game.create_children(leaf_node.position, leaf_node.is_first_player_move)
-
-    for next_position in next_positions:
-        new_node = Node(next_position)
-        new_node.parent = leaf_node
-        new_node.is_first_player_move = not leaf_node.is_first_player_move
-        leaf_node.children.append(new_node)
-
-    next_child = random.choice(leaf_node.children)
-    return next_child
+        if debug:
+            self.print_tree(root)
 
 
-def simulate(node, game):
-    outcome = game.simulate_to_the_end(node.position, node.is_first_player_move)
-    return outcome
+    def select(self, node):
+        pass
 
 
-def backpropagate(node, outcome):
-    node.n_visits += 1
-    if node.is_first_player_move and outcome == FIRST_PLAYER_WIN:
-        node.value += 1
-    if not node.is_first_player_move and outcome == SECOND_PLAYER_WIN:
-        node.value += 1
-    if outcome == DRAW:
-        node.value += 0.5
-
-    if node.parent is not None:
-        backpropagate(node.parent, outcome)
+    def simulate(self, node):
+        pass
 
 
-# def print_tree(node):
-#     print(node.position)
-#     for c in node.children:
-#         print_tree(c)
+    def backpropagate(self, node, outcome):
+        pass
 
-def print_tree(node, level=0):
-    print('{}{}'.format('\t' * level, node.position))
-    for child in node.children:
-        print_tree(child, level + 1)
+
+    def print_tree(self, node, level=0):
+        print('{}{}'.format('\t' * level, node.position))
+        for child in node.children:
+            self.print_tree(child, level + 1)
+
+
+class MonteCarloTreeSearch(TreeSearch):
+    def __init__(self, game):
+        super().__init__(game)
+
+
+    def select(self, node):
+        if len(node.children) == 0:
+            # found a leaf, return it
+            return node
+
+        non_terminal_children = [child for child in node.children if not self.game.is_terminal(child.position)]
+        # sort in decsending order
+        non_terminal_children.sort(key=lambda c: c.ucb(), reverse=True)
+
+        # iterate over sorted children, finish when
+        # we found a non-terminal leaf
+        for child in non_terminal_children:
+            leaf = self.select(child)
+            if leaf is not None:
+                return leaf
+
+        # we haven't found a non-terminal leaf in this subtree
+        return None
+
+
+    def expand(self, leaf_node):
+        next_positions = self.game.create_children(leaf_node.position, leaf_node.is_first_player_move)
+
+        for next_position in next_positions:
+            new_node = Node(next_position)
+            new_node.parent = leaf_node
+            new_node.is_first_player_move = not leaf_node.is_first_player_move
+            leaf_node.children.append(new_node)
+
+        next_child = random.choice(leaf_node.children)
+        return next_child
+
+
+    def simulate(self, node):
+        outcome = self.game.simulate_to_the_end(node.position, node.is_first_player_move)
+        return outcome
+
+
+    def backpropagate(self, node, outcome):
+        node.n_visits += 1
+        if node.is_first_player_move and outcome == FIRST_PLAYER_WIN:
+            node.value += 1
+        if not node.is_first_player_move and outcome == SECOND_PLAYER_WIN:
+            node.value += 1
+        if outcome == DRAW:
+            node.value += 0.5
+
+        if node.parent is not None:
+            self.backpropagate(node.parent, outcome)
 
 
 if __name__ == "__main__":
     game = TicTacToeGame()
-    loop(game, 100, True)
+    mcts = MonteCarloTreeSearch(game)
+    mcts.loop(5, True)
 
     # tttg = TicTacToeGame(board_size=4)
     # print(tttg.simulate_to_the_end(tttg.get_initial_position(), True))
