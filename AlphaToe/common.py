d35548b605183fe16c469578fd948d33ff792a82
Andrei Paleyes <apalees@gmail.com>
2019-10-12 10:21:36 +0100

    Moved interfaces and base classes to separate file

---
+++ AlphaToe/common.py
@@ -0,0 +1,83 @@
+import math
+
+FIRST_PLAYER_WIN = 1
+SECOND_PLAYER_WIN = -1
+DRAW = 0
+
+UCB_C = 1.0
+
+
+class Game:
+    def get_initial_position(self):
+        raise NotImplementedError
+
+    def create_children(self, position, is_first_player_move):
+        raise NotImplementedError
+
+    def simulate_to_the_end(self, position, is_first_player_move):
+        raise NotImplementedError
+
+    def is_terminal(self, position):
+        raise NotImplementedError
+
+
+class Node:
+    def __init__(self, position):
+        self.position = position
+        self.value = 0
+        self.n_visits = 0
+        self.children = []
+        self.parent = None
+        self.is_first_player_move = True
+
+    def ucb(self):
+        v = self.value
+        n = self.n_visits
+        N = self.parent.n_visits
+
+        if n == 0:
+            n = 1
+
+        return v / n + UCB_C * math.sqrt(math.log(N) / n)
+
+
+class TreeSearch:
+    def __init__(self, game):
+        self.game = game
+
+
+    def loop(self, n_iterations, debug=False):
+        root = Node(self.game.get_initial_position())
+
+        for i in range(n_iterations):
+            print("Starting iteration " + str(i))
+
+            leaf_node = self.select(root)
+            if leaf_node is None:
+                print("All possible positions were evaluated")
+                break
+
+            new_child = self.expand(leaf_node)
+            outcome = self.simulate(new_child)
+            self.backpropagate(new_child, outcome)
+
+        if debug:
+            self.print_tree(root)
+
+
+    def select(self, node):
+        raise NotImplementedError
+
+
+    def simulate(self, node):
+        raise NotImplementedError
+
+
+    def backpropagate(self, node, outcome):
+        raise NotImplementedError
+
+
+    def print_tree(self, node, level=0):
+        print('{}{}'.format('\t' * level, node.position))
+        for child in node.children:
+            self.print_tree(child, level + 1)
--- AlphaToe/mcts.py
+++ AlphaToe/mcts.py
@@ -4,15 +4,10 @@
 import math
 import random
 
+from common import FIRST_PLAYER_WIN, DRAW, SECOND_PLAYER_WIN
+from common import Game, Node, TreeSearch
 
-FIRST_PLAYER_WIN = 1
-SECOND_PLAYER_WIN = -1
-DRAW = 0
-
-UCB_C = 1.0
-
-
-class MockGame:
+class MockGame(Game):
     def __init__(self, n_children=3):
         self.n_children = n_children
 
@@ -39,7 +34,7 @@ class MockGame:
         return True
 
 
-class TicTacToeGame:
+class TicTacToeGame(Game):
     def __init__(self, board_size=3, win_count=3):
         self.board_size = board_size
         self.win_count = win_count
@@ -155,68 +150,6 @@ class TicTacToeGame:
         for line in board:
             print(''.join(line))
         print('\n')
-
-
-class Node:
-    def __init__(self, position):
-        self.position = position
-        self.value = 0
-        self.n_visits = 0
-        self.children = []
-        self.parent = None
-        self.is_first_player_move = True
-
-    def ucb(self):
-        v = self.value
-        n = self.n_visits
-        N = self.parent.n_visits
-
-        if n == 0:
-            n = 1
-
-        return v / n + UCB_C * math.sqrt(math.log(N) / n)
-
-
-class TreeSearch:
-    def __init__(self, game):
-        self.game = game
-
-
-    def loop(self, n_iterations, debug=False):
-        root = Node(self.game.get_initial_position())
-
-        for i in range(n_iterations):
-            print("Starting iteration " + str(i))
-
-            leaf_node = self.select(root)
-            if leaf_node is None:
-                print("All possible positions were evaluated")
-                break
-
-            new_child = self.expand(leaf_node)
-            outcome = self.simulate(new_child)
-            self.backpropagate(new_child, outcome)
-
-        if debug:
-            self.print_tree(root)
-
-
-    def select(self, node):
-        pass
-
-
-    def simulate(self, node):
-        pass
-
-
-    def backpropagate(self, node, outcome):
-        pass
-
-
-    def print_tree(self, node, level=0):
-        print('{}{}'.format('\t' * level, node.position))
-        for child in node.children:
-            self.print_tree(child, level + 1)
 
 
 class MonteCarloTreeSearch(TreeSearch):
