9e01d20df94f5a1d90ad6587d288674c53a0aea3
Andrei Paleyes <paleyes@amazon.com>
2019-12-19 02:22:03 +0000

    Adventures in training nn

--- AlphaToe/train_nn.py
+++ AlphaToe/train_nn.py
@@ -39,23 +39,37 @@ def build_model_position_and_move(x, n_input):
     return output_layer
 
 def build_model_position(x, n_input):
-    n_hidden1 = 50
+    n_hidden1 = 500
+    n_hidden2 = 500
+    n_hidden3 = 500
 
     weights = {
         'h1': tf.Variable(tf.random.normal([n_input, n_hidden1]), name='w_h_1'),
-        'out': tf.Variable(tf.random.normal([n_hidden1, 1]), name='w_out')
+        'h2': tf.Variable(tf.random.normal([n_hidden1, n_hidden2]), name='w_h_2'),
+        'h3': tf.Variable(tf.random.normal([n_hidden2, n_hidden3]), name='w_h_3'),
+        'out': tf.Variable(tf.random.normal([n_hidden3, 1]), name='w_out')
     }
 
     biases = {
         'b1': tf.Variable(tf.random.normal([n_hidden1]), name='b_h_1'),
+        'b2': tf.Variable(tf.random.normal([n_hidden2]), name='b_h_2'),
+        'b3': tf.Variable(tf.random.normal([n_hidden3]), name='b_h_3'),
         'out': tf.Variable(tf.random.normal([1]), name='b_out')
     }
 
     layer_1 = tf.add(tf.matmul(x, weights['h1']), biases['b1'], name='hidden_layer_1')
-    layer_1 = tf.nn.relu(layer_1)
+    layer_1 = tf.nn.tanh(layer_1)
     layer_1 = tf.nn.dropout(layer_1, rate=0.1)
 
-    output_layer = tf.add(tf.matmul(layer_1, weights['out']), biases['out'], name='output_layer')
+    layer_2 = tf.add(tf.matmul(layer_1, weights['h2']), biases['b2'], name='hidden_layer_2')
+    layer_2 = tf.nn.tanh(layer_2)
+    layer_2 = tf.nn.dropout(layer_2, rate=0.1)
+
+    layer_3 = tf.add(tf.matmul(layer_2, weights['h3']), biases['b3'], name='hidden_layer_3')
+    layer_3 = tf.nn.tanh(layer_3)
+    layer_3 = tf.nn.dropout(layer_3, rate=0.1)
+
+    output_layer = tf.add(tf.matmul(layer_3, weights['out']), biases['out'], name='output_layer')
     output_layer = tf.math.tanh(output_layer)
 
     return output_layer
@@ -148,17 +162,69 @@ if __name__ == '__main__':
 
         y = tf.placeholder('float')
         loss = tf.reduce_mean(tf.square(model.model - y))
-        optimizer = tf.train.GradientDescentOptimizer(0.1).minimize(loss)
+        optimizer = tf.train.AdamOptimizer(0.01).minimize(loss)
 
         tf_session.run(tf.global_variables_initializer())
 
-        n_training_games = 1000
+        games_per_round = 300
+        n_rounds = 5
+        batch_size = 100
+        n_epochs = 10
+        n_iterations = 100
+        n_training_games = games_per_round * n_rounds
+
+        # for round_i in range(n_rounds):
+        #     print("Starting round", round_i)
+
+        #     # collect data phase
+        #     print("Played games:", end=" ", flush=True)
+        #     training_x = []
+        #     training_y = []
+        #     for game_i in range(games_per_round):
+        #         game_training_x = []
+        #         position = game.get_initial_position()
+        #         outcome = None
+        #         while outcome is None:
+        #             new_position = mcts.loop(n_iterations, initial_position=position)
+        #             move = game.get_move(position, new_position)
+
+        #             model_input = game.position_to_model_input(position) + game.move_to_model_input(move)
+        #             game_training_x.append(model_input)
+
+        #             position = new_position
+        #             outcome = game.find_outcome(position)
+
+        #         game_training_y = [outcome] * len(game_training_x)
+
+        #         training_x.extend(game_training_x)
+        #         training_y.extend(game_training_y)
+
+        #         if game_i % 100 == 0:
+        #             print(game_i, end=" ", flush=True)
+
+        #     # training phase
+        #     print("Starting training phase")
+        #     for epoch in range(n_epochs):
+        #         epoch_loss = 0
+        #         for start_i in range(0, len(training_x) - batch_size, batch_size):
+        #             epoch_x = training_x[start_i : start_i + batch_size]
+        #             epoch_y = training_y[start_i : start_i + batch_size]
+        #             _, cl = tf_session.run([optimizer, loss], feed_dict={model.x: epoch_x, y: epoch_y})
+        #             epoch_loss += cl
+
+        #         print('Epoch', epoch, 'completed out of', n_epochs, ', loss:', epoch_loss)
+
+
         for game_i in range(n_training_games):
             training_data = []
             outcome = None
             position = game.get_initial_position()
             while outcome is None:
-                new_position = mcts.loop(3, initial_position=position)
+                try:
+                    new_position = mcts.loop(n_iterations, initial_position=position)
+                except:
+                    print(position)
+                    raise
                 move = game.get_move(position, new_position)
 
                 model_input = game.position_to_model_input(position) + game.move_to_model_input(move)
@@ -172,12 +238,17 @@ if __name__ == '__main__':
             if game_i % 200 == 0:
                 print("Played {} games, current loss is {}".format(game_i, current_loss))
 
+        def get_score(p, m):
+            model_input = game.position_to_model_input(p) + game.move_to_model_input(m)
+            score = model.evaluate(model_input)
+            return score
+
         print()
         print("Now let's play a test game")
         outcome = None
         position = game.get_initial_position()
         while outcome is None:
-            position = mcts.loop(3, initial_position=position)
+            position = mcts.loop(n_iterations, initial_position=position)
             print()
             print(position[0:3], '\n', position[3:6], '\n', position[6:9])
             outcome = game.find_outcome(position)
